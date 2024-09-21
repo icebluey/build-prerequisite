28c1e39067f19dda884c4a2f478a5939dd6426a1
Kenneth Davidson <xylene2016@protonmail.com>
2024-09-21 08:37:39 -0400

    Update build-chrony.sh

--- ub2204/scripts/build-chrony.sh
+++ ub2204/scripts/build-chrony.sh
@@ -25,8 +25,88 @@ export CXX
 
 set -e
 
-_build_zstd() {
+_strip_files() {
+    if [[ "$(pwd)" = '/' ]]; then
+        echo
+        printf '\e[01;31m%s\e[m\n' "Current dir is '/'"
+        printf '\e[01;31m%s\e[m\n' "quit"
+        echo
+        exit 1
+    else
+        rm -fr lib64
+        rm -fr lib
+        chown -R root:root ./
+    fi
+    find usr/ -type f -iname '*.la' -delete
+    if [[ -d usr/share/man ]]; then
+        find -L usr/share/man/ -type l -exec rm -f '{}' \;
+        sleep 2
+        find usr/share/man/ -type f -iname '*.[1-9]' -exec gzip -f -9 '{}' \;
+        sleep 2
+        find -L usr/share/man/ -type l | while read file; do ln -svf "$(readlink -s "${file}").gz" "${file}.gz" ; done
+        sleep 2
+        find -L usr/share/man/ -type l -exec rm -f '{}' \;
+    fi
+    if [[ -d usr/lib/x86_64-linux-gnu ]]; then
+        find usr/lib/x86_64-linux-gnu/ -type f \( -iname '*.so' -or -iname '*.so.*' \) | xargs --no-run-if-empty -I '{}' chmod 0755 '{}'
+        find usr/lib/x86_64-linux-gnu/ -iname 'lib*.so*' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+        find usr/lib/x86_64-linux-gnu/ -iname '*.so' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    if [[ -d usr/lib64 ]]; then
+        find usr/lib64/ -type f \( -iname '*.so' -or -iname '*.so.*' \) | xargs --no-run-if-empty -I '{}' chmod 0755 '{}'
+        find usr/lib64/ -iname 'lib*.so*' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+        find usr/lib64/ -iname '*.so' -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    if [[ -d usr/sbin ]]; then
+        find usr/sbin/ -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    if [[ -d usr/bin ]]; then
+        find usr/bin/ -type f -exec file '{}' \; | sed -n -e 's/^\(.*\):[  ]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' /usr/bin/strip '{}'
+    fi
+    echo
+}
 
+_build_libedit() {
+    /sbin/ldconfig >/dev/null 2>&1
+    set -e
+    _tmp_dir="$(mktemp -d)"
+    cd "${_tmp_dir}"
+    _libedit_ver="$(wget -qO- 'https://www.thrysoee.dk/editline/' | grep libedit-[1-9].*\.tar | sed 's|"|\n|g' | grep '^libedit-[1-9]' | sed -e 's|\.tar.*||g' -e 's|libedit-||g' | sort -V | uniq | tail -n 1)"
+    wget -c -t 9 -T 9 "https://www.thrysoee.dk/editline/libedit-${_libedit_ver}.tar.gz"
+    tar -xof libedit-*.tar.*
+    sleep 1
+    rm -f libedit-*.tar*
+    cd libedit-*
+    sed -i "s/lncurses/ltinfo/" configure
+    LDFLAGS='' ; LDFLAGS="${_ORIG_LDFLAGS}"' -Wl,-rpath,\$$ORIGIN' ; export LDFLAGS
+    ./configure \
+    --build=x86_64-linux-gnu \
+    --host=x86_64-linux-gnu \
+    --prefix=/usr \
+    --libdir=/usr/lib/x86_64-linux-gnu \
+    --includedir=/usr/include \
+    --sysconfdir=/etc \
+    --enable-shared --enable-static \
+    --enable-widec
+    sleep 1
+    make -j2 all
+    rm -fr /tmp/libedit
+    make install DESTDIR=/tmp/libedit
+    cd /tmp/libedit
+    _strip_files
+    install -m 0755 -d usr/lib/x86_64-linux-gnu/chrony/private
+    cp -af usr/lib/x86_64-linux-gnu/*.so* usr/lib/x86_64-linux-gnu/chrony/private/
+    rm -f /usr/lib/x86_64-linux-gnu/libedit.*
+    sleep 2
+    /bin/cp -afr * /
+    sleep 2
+    cd /tmp
+    rm -fr "${_tmp_dir}"
+    rm -fr /tmp/libedit
+    /sbin/ldconfig
+}
+
+_build_zstd() {
 LDFLAGS=''
 LDFLAGS="${_ORIG_LDFLAGS}"
 export LDFLAGS
@@ -534,6 +614,7 @@ echo
 
 cd /tmp
 rm -fr /usr/lib/x86_64-linux-gnu/chrony
+_build_libedit
 _build_zstd
 _build_brotli
 _build_nettle
