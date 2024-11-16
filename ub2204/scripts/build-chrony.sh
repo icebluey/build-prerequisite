87ebf64bb05360b64440ca81f87ef4c50425c88a
Kenneth Davidson <xylene2016@protonmail.com>
2024-11-16 06:03:49 -0500

    Update build-chrony.sh

--- ub2204/scripts/build-chrony.sh
+++ ub2204/scripts/build-chrony.sh
@@ -263,7 +263,7 @@ else
     -DLIB_SUFFIX=64 \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DCMAKE_INSTALL_SO_NO_EXE:INTERNAL=0
-    cmake --build "build"  --verbose
+    cmake --build "build" --parallel $(nproc --all) --verbose
     rm -fr /tmp/brotli
     DESTDIR="/tmp/brotli" cmake --install "build"
 fi
