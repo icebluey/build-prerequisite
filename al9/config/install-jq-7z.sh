b67f67ea54c1d74df7c2fa748a78bfff37f666f9
Kenneth Davidson <xylene2016@protonmail.com>
2025-03-14 11:05:19 -0400

    Update and rename install-7z-static.sh to install-jq-7z.sh

--- al9/config/install-jq-7z.sh
+++ al9/config/install-jq-7z.sh
@@ -1,6 +1,15 @@
 set -e
 _tmp_dir="$(mktemp -d)"
 cd "${_tmp_dir}"
+wget -c -t 9 -T 9 'https://github.com/jqlang/jq/releases/download/jq-1.7.1/jq-linux-amd64'
+sleep 1
+mv jq-linux-amd64 jq
+rm -f /usr/bin/jq
+rm -f /usr/local/bin/jq
+install -v -c -m 0755 jq /usr/bin/jq
+sleep 1
+rm -fr jq*
+
 _7zip_loc=$(wget -qO- 'https://www.7-zip.org/download.html' | grep -i '\-linux-x64.tar' | grep -i 'href="' | sed 's|"|\n|g' | grep -i '\-linux-x64.tar' | sort -V | tail -n 1)
 _7zip_ver=$(echo ${_7zip_loc} | sed -e 's|.*7z||g' -e 's|-linux.*||g')
 wget -c -t 9 -T 9 "https://www.7-zip.org/${_7zip_loc}"
@@ -8,20 +17,10 @@ sleep 1
 tar -xof *.tar.*
 sleep 1
 rm -f *.tar*
-
+rm -f /usr/bin/7z
+rm -f /usr/local/bin/7z
 install -v -c -m 0755 7zzs /usr/bin/7z
-cd /tmp
 sleep 1
+cd /tmp
 rm -fr "${_tmp_dir}"
 exit
-
-mkdir /tmp/7-zip-"${_7zip_ver}"-static
-install -v -c -m 0755 7zzs /tmp/7-zip-"${_7zip_ver}"-static/7z
-cd /tmp
-sleep 1
-tar -cvf 7-zip-"${_7zip_ver}"-static.tar 7-zip-"${_7zip_ver}"-static
-sleep 1
-rm -fr "${_tmp_dir}"
-rm -fr /tmp/7-zip-"${_7zip_ver}"-static
-exit
-
