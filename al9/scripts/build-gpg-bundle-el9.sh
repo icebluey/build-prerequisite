f78782c6bf7e540d1b7384e6a415bdc5c32d1997
Kenneth Davidson <xylene2016@protonmail.com>
2025-10-25 03:36:54 -0400

    Update patch download links in build script

--- al9/scripts/build-gpg-bundle-el9.sh
+++ al9/scripts/build-gpg-bundle-el9.sh
@@ -347,10 +347,11 @@ rm -fr pinentry-*
 cd gnupg-*
 
 # patch
-#rm -f /tmp/p*.patch
-#wget 'https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=patch;h=a73c88817ce2dc05d4eefc2a8f31b89504523a9a' -O /tmp/p01.patch
-#ls -1 /tmp/p*.patch | xargs -I '{}' patch -N -p1 -i '{}'
-#rm -f /tmp/p*.patch
+rm -f /tmp/p*.patch
+wget 'https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=patch;h=115d138ba599328005c5321c0ef9f00355838ca9' -O /tmp/p01.patch
+wget 'https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=patch;h=6570700fddcb92ce08024a651dd6989025fe9e20' -O /tmp/p02.patch
+ls -1 /tmp/p*.patch | xargs -I '{}' patch -N -p1 -i '{}'
+rm -f /tmp/p*.patch
 
 ./configure \
 --build=x86_64-linux-gnu \
