46db0fad38079378ba12cbc86aae9032c81abb9f
Kenneth Davidson <xylene2016@protonmail.com>
2026-01-29 17:38:39 -0500

    Update install-jq-7z.sh

--- ub2204/config/install-jq-7z.sh
+++ ub2204/config/install-jq-7z.sh
@@ -1,26 +1,44 @@
-set -e
-_tmp_dir="$(mktemp -d)"
-cd "${_tmp_dir}"
-wget -c -t 9 -T 9 'https://github.com/jqlang/jq/releases/download/jq-1.8.1/jq-linux-amd64'
-sleep 1
-mv jq-linux-amd64 jq
-rm -f /usr/bin/jq
-rm -f /usr/local/bin/jq
-install -v -c -m 0755 jq /usr/bin/jq
-sleep 1
-rm -fr jq*
+#!/bin/bash
+set -euo pipefail
+_install_jq() {
+    set -euo pipefail
+    local _tmp_dir="$(mktemp -d)"
+    cd "${_tmp_dir}"
+    _jq_url="$(wget -qO- 'https://jqlang.org/download/' | grep 'jq-linux-amd64' | sed 's/"/\n/g' | grep 'http.*jq-linux-amd64' | sort -V | tail -n 1)"
+    wget -q -c -t 9 -T 9 "${_jq_url}" -O jq-linux-amd64
+    mv jq-linux-amd64 jq
+    chmod 0755 jq
+    file jq | sed -n -E 's/^(.*):[[:space:]]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' strip '{}'
+    rm -f /usr/bin/jq /usr/local/bin/jq
+    install -v -c -m 0755 jq /usr/bin/jq
+    cp -f /usr/bin/jq /usr/local/bin/jq
+    /usr/bin/jq --version 2>/dev/null || true
+    cd /tmp
+    rm -fr "${_tmp_dir}"
+}
+_install_jq
 
-_7zip_loc=$(wget -qO- 'https://www.7-zip.org/download.html' | grep -i '\-linux-x64.tar' | grep -i 'href="' | sed 's|"|\n|g' | grep -i '\-linux-x64.tar' | sort -V | tail -n 1)
-_7zip_ver=$(echo ${_7zip_loc} | sed -e 's|.*7z||g' -e 's|-linux.*||g')
-wget -c -t 9 -T 9 "https://www.7-zip.org/${_7zip_loc}"
-sleep 1
-tar -xof *.tar.*
-sleep 1
-rm -f *.tar*
-rm -f /usr/bin/7z
-rm -f /usr/local/bin/7z
-install -v -c -m 0755 7zzs /usr/bin/7z
-sleep 1
-cd /tmp
-rm -fr "${_tmp_dir}"
+_install_7z() {
+    set -euo pipefail
+    local _tmp_dir="$(mktemp -d)"
+    cd "${_tmp_dir}"
+    #_7zip_loc="$(wget -qO- 'https://www.7-zip.org/download.html' | grep -i '\-linux-x64.tar' | grep -i 'href="' | sed 's|"|\n|g' | grep -i '\-linux-x64.tar' | sort -V | tail -n 1)"
+    #wget -q -c -t 9 -T 9 "https://www.7-zip.org/${_7zip_loc}"
+    #tar -xof *.tar*
+    #sleep 1
+    #rm -f *.tar*
+    #file 7zzs | sed -n -E 's/^(.*):[[:space:]]*ELF.*, not stripped.*/\1/p' | xargs --no-run-if-empty -I '{}' strip '{}'
+    #rm -f 7z && mv 7zzs 7z
+    wget -q -c -t 9 -T 9 'https://github.com/icebluey/7zip-zstd/releases/latest/download/7z.tar'
+    wget -q -c -t 9 -T 9 'https://github.com/icebluey/7zip-zstd/releases/latest/download/7z.tar.sha256'
+    sha256sum -c 7z.tar.sha256
+    tar -xof 7z.tar
+    rm -f /usr/bin/7z /usr/local/bin/7z
+    install -v -c -m 0755 7z /usr/bin/7z
+    cp -f /usr/bin/7z /usr/local/bin/7z
+    /usr/bin/7z --version 2>/dev/null || true
+    cd /tmp
+    rm -fr "${_tmp_dir}"
+}
+_install_7z
 exit
